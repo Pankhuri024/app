90bb93355785e420c4feb10547326f7014cac295
Pankhuri024 <pankhurivapta@gmail.com>
2024-08-14 19:23:06 +0530

    Add script a new

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import json
 import re
 import logging
 import sys
-
+import nltk
 
 logging.basicConfig(level=logging.DEBUG,  # Change to DEBUG to get detailed logs
                     format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
@@ -32,6 +32,15 @@ ALLOWED_IP_ADDRESSES = {"127.0.0.1","171.50.226.59"}  # Add allowed IP addresses
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20 MB
 
+nltk.download('punkt_tab', download_dir='/app/download_nltk_data')  # Specify a directory
+@app.route('/test-nltk-data', methods=['GET'])
+def test_nltk_data():
+    try:
+        import nltk
+        nltk.data.find('tokenizers/punkt_tab/english.pickle')
+        return jsonify({'Message': 'NLTK data is available'}), 200
+    except LookupError:
+        return jsonify({'Message': 'NLTK data not found'}), 500
 
 
 def allowed_file(filename):
